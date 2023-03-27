65b28f67267cdcf495423d2dc360784bab6ce759
Torben <torben-voigt@gmx.de>
2023-03-27 13:17:38 +0200

    Aufgabe 1

---
+++ fhdw_drivers_sensors/Converter.java
@@ -0,0 +1,11 @@
+package fhdw_drivers_sensors;
+public class Converter{
+
+    public static double convertInKmh(double windSpeedInMs){
+        return windSpeedInMs/3.6;
+    }
+
+    public static double convertInK(double temperatureInC){
+        return temperatureInC+273.15;
+    }
+}
---
+++ fhdw_drivers_sensors/Main.java
@@ -0,0 +1,15 @@
+package fhdw_drivers_sensors;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        
+        SerialWindspeedDriver windDriver = new SerialWindspeedDriver("/dev/tty1");
+        SerialTemperatureDriver tempDriver = new SerialTemperatureDriver("/dev/tty0");
+
+        System.out.println(Converter.convertInKmh(windDriver.currentWindspeed())+" km/h");
+        System.out.println(Converter.convertInK(tempDriver.read())+" K");
+        
+    }
+}
