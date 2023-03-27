5489e5e8c12eca848ae2bf9cc010f94526f5a363
Dustin Plewa <dustin.plewa@freenet.de>
2023-03-27 15:38:40 +0200

    Added multiple classes.

--- fhdw_drivers_sensors/Converter.java
+++ fhdw_drivers_sensors/Converter.java
@@ -1,11 +1,8 @@
 package fhdw_drivers_sensors;
 public class Converter{
 
-    public static double convertInKmh(double windSpeedInMs){
-        return windSpeedInMs/3.6;
-    }
-
-    public static double convertInK(double temperatureInC){
-        return temperatureInC+273.15;
-    }
+    public static double convertInKmh(double windSpeedInMs) { return windSpeedInMs/3.6; }
+    public static double convertInK(double temperatureInC) { return temperatureInC+273.15; }
+    public static double convertUSB(int temp) { return temp * 10; }
+    
 }
--- fhdw_drivers_sensors/Main.java
+++ fhdw_drivers_sensors/Main.java
@@ -3,14 +3,19 @@ package fhdw_drivers_sensors;
 import java.io.FileNotFoundException;
 
 public class Main {
+
     public static void main(String[] args) throws FileNotFoundException {
-        
         SerialWindspeedDriver windDriver = new SerialWindspeedDriver("/dev/tty1");
         SerialTemperatureDriver tempDriver = new SerialTemperatureDriver("/dev/tty0");
-
+        USBTemperatureDriver usbTempDriver = new USBTemperatureDriver("/dev/ttyUSB");
+        SerialPressureSensor pressureSensor = new SerialPressureSensor("/dev/tty");
+        SerialRainSensor rainSensor = new SerialRainSensor("/dev/tty");
 
         PrintAdapter.print(Converter.convertInKmh(windDriver.currentWindspeed())+" km/h");
         PrintAdapter.print(Converter.convertInK(tempDriver.read())+" K");
-
+        PrintAdapter.print(Converter.convertUSB(usbTempDriver.getTemperatureValue())+"");
+        PrintAdapter.print(pressureSensor.getSensorValue()+"hPa");
+        PrintAdapter.print(rainSensor.getSensorValue()+"mm/h");
     }
+    
 }
